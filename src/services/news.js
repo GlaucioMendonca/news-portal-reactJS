45860fb1f2fd2b3225def3db3913fbd39c7ea327
Glaucio Mendonca <glaucio43@hotmail.com>
2020-08-02 15:41:33 -0300

    Use process.env to get api key

--- src/services/news.js
+++ src/services/news.js
@@ -1,9 +1,5 @@
 import Api from './api'
 
 export const getNews = async ( category, page = 0 ) => await Api.get(
-    `/top-headlines?country=br&category=${category}&pageSize=10&page=${page}&apiKey=suachave`
-)
-    
-export const getLatestNews = async () => await Api.get(
-    `/top-headlines?country=br&pageSize=10&apiKey=suachave`
+    `/top-headlines?country=br&category=${category}&pageSize=10&page=${page}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
 )
